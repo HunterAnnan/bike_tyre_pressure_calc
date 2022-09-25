0eca15b2836ffeb5c5da3a9c2efdd3543cb7fef5
Hunter <112955296+HunterAnnan@users.noreply.github.com>
2022-09-25 22:10:00 +0100

    Initial outline of variables added

--- bike_tyre_p_calc.py
+++ bike_tyre_p_calc.py
@@ -7,3 +7,4 @@ Tool for calculating tyre pressures for a given tyre width and rider weight.
 tyre_width = input("Tyre width:\n")
 tyre_size = input("Tyre size:\n")
 rider_weight = input("Weight:\n")
+tubeless = input("Tubeless or with inner tube?:\n")
